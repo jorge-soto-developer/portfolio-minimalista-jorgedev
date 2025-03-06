1c2ec6462818470914e513a6d25ce7fae13d8ab0
jorge-soto-developer <jorgeignacio68@gmail.com>
2025-03-05 21:49:03 -0300

    Conecté Vercel con el repositorio

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -152,5 +152,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("./../src/content.config.mj");
+	export type ContentConfig = typeof import("./../src/content.config.mjs");
 }
